65c48373d0ef3345ca98d08bacac9584a87c5d81
AHMAD MUQRI BIN AHMAD BADRI <ahmadmuqriz95@gmail.com>
2019-11-17 14:14:16 +0800

    newly update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Speedo Distancioog
                 </div>
 
                 <div class="links">
